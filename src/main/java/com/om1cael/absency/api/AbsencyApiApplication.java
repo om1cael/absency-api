c7eabf0c07b2e21e9f92e1a4232c62a3a331e589
om1cael <188113439+om1cael@users.noreply.github.com>
2025-01-29 17:51:48 -0300

    Initial commit

---
+++ src/main/java/com/om1cael/absency/api/AbsencyApiApplication.java
@@ -0,0 +1,13 @@
+package com.om1cael.absency.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsencyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsencyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/om1cael/absency/api/AbsencyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om1cael.absency.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsencyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
